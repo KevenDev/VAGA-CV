1d663e6b6c662a44ba613e3c0ecab98dda84e54b
KevenArbache <keven.teles@arbache.com.br>
2023-08-15 23:18:54 -0300

    telas mobile e desktop

---
+++ js/app.js
@@ -0,0 +1,134 @@
+import dataImages from "./mockData.js";
+const navBtn = document.querySelector("#navBtn");
+const closeBtn = document.querySelector("#closeBtn");
+const sidebar = document.querySelector("#sidebarMobile");
+const cardsGroup = document.querySelector(".cardsGroup");
+const tableGroup = document.querySelector(".table")
+const blocks = document.querySelector("#cardGray");
+const sold = document.querySelector("#cardRed");
+const reserved = document.querySelector("#cardOrange");
+const available = document.querySelector("#cardGreen");
+
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+
+blocks.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+})
+
+blocks.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+  filterResultsByAvailability("bloqueada");
+});
+
+sold.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+  filterResultsByAvailability("vendida");
+});
+
+reserved.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+  filterResultsByAvailability("reservada");
+});
+
+available.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+  filterResultsByAvailability("disponivel");
+});
+
+const cardToHtml = (data) => {
+  const card = document.createElement('aside');
+  card.className = "card";
+  card.innerHTML = `
+  <div class="cardHeader">
+  <p>${data.bloco}</p>
+</div>
+<div class="card-footer">
+  <div class="spaceValues">
+    <div class="">
+      <span> Quartos: </span>
+    </div>
+    <div>
+    <span> ${data.qtdQuartos} </span>
+    </div>
+  </div>
+  
+  <div class="spaceValues">
+    <div>
+    <span>  Andar: </span>
+    </div>
+    <div>
+      ${data.andar}
+    </div>
+  </div>
+  <div class="spaceValues">
+    <div>
+      Área:
+    </div>
+    <div>
+      ${data.areaTotal}
+    </div>
+  </div>
+  <div class="spaceValues">
+    <div>
+      Coluna:
+    </div>
+    <div>
+     ${data.coluna}
+    </div>
+  </div>
+</div>
+ <div class="disponibility ${data.estado}">
+    <span>${data.estado}</span>
+  </div>
+  `;
+  return card;
+}
+
+const tableToHtml = (data) => {
+  const table = document.createElement('tr');
+  table.className = "tableStyle"
+  table.innerHTML = `
+    <td class="roundedTable detailsTable">${data.bloco}</td>
+    <td class="detailsTable">${data.qtdQuartos}</td>
+    <td class="detailsTable">${data.andar}</td>
+    <td class="detailsTable">${data.areaTotal}</td>
+    <td>${data.coluna}</td>
+    <td class="roundedTableEnd ${data.estado}">${data.estado}</td>
+  `;
+  return table;
+}
+
+
+dataImages.map(data => {
+  const card = cardToHtml(data);
+  const table = tableToHtml(data)
+  cardsGroup.appendChild(card)
+  tableGroup.appendChild(table)
+})
+
+const filterResultsByAvailability = (availability) => {
+  const filteredResults = dataImages.filter(data => data.estado.toLowerCase() === availability);
+  cardsGroup.innerHTML = "";
+  tableGroup.innerHTML = `  
+    <tr>
+      <th>BLOCO</th>
+      <th>QTD. QUARTOS</th>
+      <th>ANDAR</th>
+      <th>ÁREA TOTAL</th>
+      <th>COLUNA</th>
+      <th>ESTADO</th>
+    </tr>`;
+
+  filteredResults.forEach(data => {
+    const card = cardToHtml(data);
+    const table = tableToHtml(data)
+    cardsGroup.appendChild(card)
+    tableGroup.appendChild(table)
+  });
+};
---
+++ js/mockData.js
@@ -0,0 +1,109 @@
+const dataImages = [
+  {
+    bloco: 'MARAGOGI',
+    qtdQuartos: '3',
+    andar: '2',
+    areaTotal: "32,99 M2",
+    coluna: '15',
+    estado: 'disponivel'
+  },
+  {
+    bloco: 'TRANCOSO',
+    qtdQuartos: '3',
+    andar: '3',
+    areaTotal: "32,99 M2",
+    coluna: '12',
+    estado: 'disponivel'
+  },
+  {
+    bloco: 'NORONHA',
+    qtdQuartos: '2',
+    andar: '5',
+    areaTotal: "54,58 M2",
+    coluna: '13',
+    estado: 'reservada'
+  },
+  {
+    bloco: 'MARAGOGI',
+    qtdQuartos: '2',
+    andar: '5',
+    areaTotal: "32,99 M2",
+    coluna: '14',
+    estado: 'vendida'
+  },
+  {
+    bloco: 'ARRAIAL DO CABO',
+    qtdQuartos: '2',
+    andar: '6',
+    areaTotal: "54,58 M2",
+    coluna: '13',
+    estado: 'disponivel'
+  },
+  {
+    bloco: 'CARNEIROS',
+    qtdQuartos: '2',
+    andar: '2',
+    areaTotal: "46,59 M2",
+    coluna: '15',
+    estado: 'vendida'
+  },
+  {
+    bloco: 'TRANCOSO',
+    qtdQuartos: '4',
+    andar: '5',
+    areaTotal: "32,99 M2",
+    coluna: '14',
+    estado: 'bloqueada'
+  },
+  {
+    bloco: 'MARAGOGI',
+    qtdQuartos: '2',
+    andar: '5',
+    areaTotal: "32,99 M2",
+    coluna: '14',
+    estado: 'bloqueada'
+  },
+  {
+    bloco: 'MARAGOGI',
+    qtdQuartos: '3',
+    andar: '7',
+    areaTotal: "32,99 M2",
+    coluna: '14',
+    estado: 'reservada'
+  },
+  {
+    bloco: 'TRANCOSO',
+    qtdQuartos: '3',
+    andar: '8',
+    areaTotal: "46,59 M2",
+    coluna: '16',
+    estado: 'vendida'
+  },
+  {
+    bloco: 'TRANCOSO',
+    qtdQuartos: '2',
+    andar: '5',
+    areaTotal: "32,99 M2",
+    coluna: '14',
+    estado: 'bloqueada'
+  },
+  {
+    bloco: 'MARAGOGI',
+    qtdQuartos: '3',
+    andar: '7',
+    areaTotal: "32,99 M2",
+    coluna: '14',
+    estado: 'reservada'
+  },
+  {
+    bloco: 'TRANCOSO',
+    qtdQuartos: '3',
+    andar: '8',
+    areaTotal: "46,59 M2",
+    coluna: '16',
+    estado: 'vendida'
+  },
+
+]
+
+export default dataImages;
